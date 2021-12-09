c41c5348e147b5494d220aedb4f58cb81261c63b
pepze21 <pepze21@gmail.com>
2021-12-10 01:11:10 +0900

    sol of remove-couple

---
+++ remove_couple4.js
@@ -0,0 +1,17 @@
+// stack을 이용한 풀이. array를 stack처럼 이용(.push() .pop())
+
+function solution(s) {
+  var arr = new Array(s[0]);
+  for (var i = 1; i < s.length; i++) {
+    if (s[i] !== arr[arr.length - 1]) {
+      arr.push(s[i]);
+    } else {
+      arr.pop();
+    }
+  }
+  if (arr.length === 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
