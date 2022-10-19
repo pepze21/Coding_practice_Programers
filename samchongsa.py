a6e82d191cf0abc5d5f87b27b14f554b49fdcaab
pepze21 <pepze21@gmail.com>
2022-10-20 01:04:11 +0900

    samchongsa python lv1

---
+++ samchongsa.py
@@ -0,0 +1,9 @@
+from itertools import combinations
+
+def solution(number):
+    answer = 0
+    for item in combinations(number, 3):
+        if (sum(item) == 0):
+            answer += 1
+    
+    return answer
